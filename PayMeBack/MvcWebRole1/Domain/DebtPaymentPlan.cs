5015e38bb5ef14d35b40b1de0b2513fd03ff9c36
Paul Glavich <glav@theglavs.com>
2012-05-03 18:55:44 +1000

    Refined domain model

---
+++ PayMeBack/MvcWebRole1/Domain/DebtPaymentPlan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Glav.PayMeBack.Web.Domain
+{
+	public class DebtPaymentPlan : BaseModel
+	{
+		public Debt DebtOwed { get; set; }
+		public PaymentPeriod PaymentPeriod { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime? ExpectedEndDate { get; set; }
+		public decimal InitialPayment { get; set; }  // How much have they paid off when the debt started?
+		public List<DebtPaymentInstallment> PaymentInstallments { get; set; }
+		public bool IsOutstanding { get; set; }  // Has it been paid off yet?
+	}
+}
