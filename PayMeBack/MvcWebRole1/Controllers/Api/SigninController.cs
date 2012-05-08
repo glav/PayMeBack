22e23a64d89d4f3ad5f63a5c59203494f68f2ae5
glav <glav@localhost>
2012-05-09 07:54:28 +1000

    Added Sign in controller and Api response model

---
+++ PayMeBack/MvcWebRole1/Controllers/Api/SigninController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Glav.PayMeBack.Web.Controllers.Api
+{
+    public class SignInController : ApiController
+    {
+    }
+}
---
+++ PayMeBack/MvcWebRole1/Models/SigninResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glav.PayMeBack.Web.Domain;
+
+namespace Glav.PayMeBack.Web.Models
+{
+	public class SignInResponse : ApiResponse
+	{
+		public User User { get; set; }
+	}
+}
