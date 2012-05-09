2132e52e1793d93f6b6eb2ec4adab285afc53249
Paul Glavich <glav@theglavs.com>
2012-05-09 18:54:04 +1000

    Ensured unit tests pass

---
+++ PayMeBack/PayMeBackWeb.UnitTests/Controllers/ApiControllerTests/MockRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glav.PayMeBack.Web.Data;
+using Glav.PayMeBack.Web.Domain;
+
+namespace PayMeBackWeb.UnitTests.Controllers.ApiControllerTests
+{
+	public class MockRepository : IRepository
+	{
+		public User GetUser(string emailAddress)
+		{
+			if (emailAddress == "exists@domain.com")
+			{
+				return new User() { EmailAddress = emailAddress, FirstNames = "Exists", Surname = "Domain", Id = Guid.NewGuid(), Password = "password" };
+			}
+
+			return null;
+		}
+
+
+		public User GetUser(Guid userId)
+		{
+			return null;
+		}
+
+
+		public Guid AddUser(User user)
+		{
+			return Guid.NewGuid();
+		}
+	}
+}
