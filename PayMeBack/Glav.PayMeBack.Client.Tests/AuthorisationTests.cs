0c7469e019eb7f697047b92db2484e023bae8a9c
glav <glav@localhost>
2012-05-25 17:07:17 +1000

    Added an Authorisation proxy

---
+++ PayMeBack/Glav.PayMeBack.Client.Tests/AuthorisationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Glav.PayMeBack.Client.Tests
+{
+	/// <summary>
+	/// Summary description for UnitTest1
+	/// </summary>
+	[TestClass]
+	public class AuthorisationTests
+	{
+		public AuthorisationTests()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			//
+			// TODO: Add test logic here
+			//
+		}
+	}
+}
---
+++ PayMeBack/MvcWebRole1/Domain/Engines/AuthenticationEngine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Glav.PayMeBack.Web.Domain.Services;
+
+namespace Glav.PayMeBack.Web.Domain.Engines
+{
+	public class AuthenticationEngine : DelegatingHandler
+	{
+		private IOAuthSecurityService _oAuthSecurityService;
+
+		public AuthenticationEngine(IOAuthSecurityService oAuthSecurityService) 
+		{
+			_oAuthSecurityService = oAuthSecurityService;
+		}
+
+		public AuthenticationEngine(HttpMessageHandler innerHandler, IOAuthSecurityService oAuthSecurityService)
+			: base(innerHandler)
+		{
+			_oAuthSecurityService = oAuthSecurityService; 
+		}
+
+		protected override System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+		{
+			if (!request.IsOAuthExplicitGrantRequest())
+			{
+				// Is it a valid token/oAuth request being made?
+				if (request.DoesContainOAuthToken())
+				{
+					var bearerToken = request.Headers.Authorization.Parameter;
+					if (!_oAuthSecurityService.IsAccessTokenValid(bearerToken))
+					{
+						return Task.Factory.StartNew(() =>
+						{
+							var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+							return response;
+						});
+
+						// there was a valid token present
+						return base.SendAsync(request, cancellationToken);
+					}
+				}
+				// There was no authorisation request we could find in the path
+				// and there was no token. So that means no access
+				return Task.Factory.StartNew(() =>
+				{
+					var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+					return response;
+				});
+
+			}
+
+			// If we get here, it is only because the access is going to an OAuth
+			// specific endpoit such as http://host/authorisation
+			// to get an access key, refresh token etc... this endpoint
+			// must allow anonymous users
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+}
