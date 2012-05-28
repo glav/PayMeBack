37dca0bcc79ae4da31f7574e3c02e67e06a16384
glav <glav@localhost>
2012-05-29 08:39:18 +1000

    Cleanup of string literals

---
+++ PayMeBack/Glav.PayMeBack.Core/OAuthErrorResponseCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glav.PayMeBack.Core
+{
+	public static class OAuthErrorResponseCode
+	{
+		public const string InvalidRequest = "invalid_request";
+		public const string InvalidClient = "invalid_client";
+		public const string InvalidGrant = "invalid_grant";
+		public const string UnauthorizedClient = "unauthorized_client";
+		public const string UnsupportedGrantType = "unsupported_grant_type";
+		public const string InvalidScope = "invalid_scope";
+	}
+}
