b00656d1acf866bcff4d483ddadf5ebe867dc39d
Paul Glavich <glav@theglavs.com>
2012-08-07 18:45:36 +1000

    improved styling on login dialog

--- PayMeBack/MvcWebRole1/Scripts/Login.js
+++ PayMeBack/MvcWebRole1/Scripts/Login.js
@@ -12,60 +12,60 @@ if (typeof window.payMeBack.login === 'undefined') {
 
 window.payMeBack.login = (function () {
 
-    var isUserSignedIn = function() {
+    var isUserSignedIn = function () {
         var state = $("#is-signed-in-state").val();
         return (typeof state !== 'undefined' && state === "true");
     };
 
-	var updateDisplayBasedOnSignedInStatus = function (isSignedIn) {
+    var updateDisplayBasedOnSignedInStatus = function (isSignedIn) {
         if (typeof isSignedIn === 'undefined') {
             isSignedIn = isUserSignedIn();
         }
-		if (isSignedIn === true) {
-			$(".hide-if-signed-in").hide();
-			$(".show-if-signed-in").show();
-		} else {
-			$(".hide-if-signed-in").show();
-			$(".show-if-signed-in").hide();
-		}
-	};
-	var submitCredentials = function (email, password, isSignup) {
-		var url, payload;
+        if (isSignedIn === true) {
+            $(".hide-if-signed-in").hide();
+            $(".show-if-signed-in").show();
+        } else {
+            $(".hide-if-signed-in").show();
+            $(".show-if-signed-in").hide();
+        }
+    };
+    var submitCredentials = function (email, password, isSignup) {
+        var url, payload;
         payload = {
             email: email,
             password: password
         };
         if (isSignup === true) {
-			url = window.payMeBack.core.makePathFromVirtual("~/membership/signup");
-		} else {
-			url = window.payMeBack.core.makePathFromVirtual("~/membership/login");
-		}
-		var jsonPayload;
-		if (typeof JSON !== 'undefined' && typeof JSON.stringify !== 'undefined') {
-			jsonPayload = JSON.stringify(payload);
-		} else {
-			jsonPayload = "";
-		}
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: jsonPayload,
-			contentType: 'application/json',
-			dataType: "json",
-			success: function (result) {
-				if (result && typeof result.error !== 'undefined') {
-					alert("The request had an error: " + result.error)
-				} else {
-					if (result && typeof result.success !== 'undefined' && result.success === true) {
-						// This worked
-						updateDisplayBasedOnSignedInStatus(true);
-						$.nyroModalRemove();
-					} else {
-						updateDisplayBasedOnSignedInStatus(false);
-						var msg = "";
-						if (typeof result.error !== 'undefined') {
-							msg = result.error;
-						}
+            url = window.payMeBack.core.makePathFromVirtual("~/membership/signup");
+        } else {
+            url = window.payMeBack.core.makePathFromVirtual("~/membership/login");
+        }
+        var jsonPayload;
+        if (typeof JSON !== 'undefined' && typeof JSON.stringify !== 'undefined') {
+            jsonPayload = JSON.stringify(payload);
+        } else {
+            jsonPayload = "";
+        }
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: jsonPayload,
+            contentType: 'application/json',
+            dataType: "json",
+            success: function (result) {
+                if (result && typeof result.error !== 'undefined') {
+                    alert("The request had an error: " + result.error)
+                } else {
+                    if (result && typeof result.success !== 'undefined' && result.success === true) {
+                        // This worked
+                        updateDisplayBasedOnSignedInStatus(true);
+                        $.nyroModalRemove();
+                    } else {
+                        updateDisplayBasedOnSignedInStatus(false);
+                        var msg = "";
+                        if (typeof result.error !== 'undefined') {
+                            msg = result.error;
+                        }
 
                         var msg;
                         if (isSignup === true) {
@@ -73,71 +73,82 @@ window.payMeBack.login = (function () {
                         } else {
                             msg = "You could not be signed in asyour credentials are not correct";
                         }
-						alert(msg);
-					}
+                        alert(msg);
+                    }
 
-				}
-			},
-			error: function () {
-				updateDisplayBasedOnSignedInStatus(false);
+                }
+            },
+            error: function () {
+                updateDisplayBasedOnSignedInStatus(false);
 
                 var msg = "There was a problem " + (isSignup ? "signing you up" : "logging you in") + " to the system. Please try again."
-				alert(msg);
-			}
-		});
-	};
+                alert(msg);
+            }
+        });
+    };
 
-	var showLoginDialog = function (redirectToHomeOnClose, isSignUp) {
-		var redirectOnClose = false;
-		var isSignupAction = false;
-		if (typeof redirectToHomeOnClose !== 'undefined') {
-			redirectOnClose = redirectToHomeOnClose === true;
-		}
-		if (typeof isSignUp !== 'undefined') {
-			isSignupAction = isSignUp === true;
-		}
+    var captureCredentialsAndSubmit = function (isSignupAction) {
+        var email = $("#credentials-userId").val();
+        var password = $("#credentials-userPassword").val();
+        submitCredentials(email, password, isSignupAction);
+    };
 
-		$.nyroModalManual(
+    var showLoginDialog = function (redirectToHomeOnClose, isSignUp) {
+        var redirectOnClose = false;
+        var isSignupAction = false;
+        if (typeof redirectToHomeOnClose !== 'undefined') {
+            redirectOnClose = redirectToHomeOnClose === true;
+        }
+        if (typeof isSignUp !== 'undefined') {
+            isSignupAction = isSignUp === true;
+        }
+
+        $.nyroModalManual(
           {
-          	url: '#credentials-dialog',
-          	minHeight: 300,
-          	height: 300,
-          	//modal: true,
-          	//closeButton: null,
-          	endRemove: function () {
-          		var credsElement = $("#credentials-container");
-          		credsElement.hide();
-          		$("li input", credsElement).val("");
-          		if (redirectOnClose) {
+              url: '#credentials-dialog',
+              minHeight: 200,
+              height: 200,
+              minWidth: 350,
+              width:380,
+              //modal: true,
+              //closeButton: null,
+              endRemove: function () {
+                  var credsElement = $("#credentials-container");
+                  credsElement.hide();
+                  $("li input", credsElement).val("");
+                  if (redirectOnClose) {
 
-          			location.assign(window.payMeBack.core.makePathFromVirtual("~"));
-          		}
+                      location.assign(window.payMeBack.core.makePathFromVirtual("~"));
+                  }
 
-          	},
-          	endShowContent: function () {
-          		$("#credentials-submit").unbind().bind("click", function () {
-          			var email = $("#credentials-userId").val();
-          			var password = $("#credentials-userPassword").val();
-          			submitCredentials(email, password, isSignupAction);
-          		});
-          		if (isSignupAction) {
-          			$("#credentials-container")
+              },
+              endShowContent: function () {
+                  $("#credentials-form input").unbind().on("keypress", function (e) {
+                      if (e.which === 13) {
+                          captureCredentialsAndSubmit(isSignupAction);
+                      }
+                  });
+                  $("#credentials-submit").unbind().bind("click", function () {
+                      captureCredentialsAndSubmit(isSignupAction);
+                  });
+                  if (isSignupAction) {
+                      $("#credentials-container")
                           .removeClass("login-dialog")
                           .addClass("signup-dialog")
                           .fadeIn();
-          		} else {
-          			$("#credentials-container")
+                  } else {
+                      $("#credentials-container")
                           .addClass("login-dialog")
                           .removeClass("signup-dialog")
                           .fadeIn();
-          		}
-          	}
+                  }
+              }
           });
-	};
+    };
 
-	return {
+    return {
         showLoginDialog: function (redirectToHomeOnClose, isSignUp) { showLoginDialog(redirectToHomeOnClose, isSignUp) },
-        updateDisplayBasedOnSignedInStatus: function(isSignedIn) { updateDisplayBasedOnSignedInStatus(isSignedIn);}
+        updateDisplayBasedOnSignedInStatus: function (isSignedIn) { updateDisplayBasedOnSignedInStatus(isSignedIn); }
     };
 })();
 
