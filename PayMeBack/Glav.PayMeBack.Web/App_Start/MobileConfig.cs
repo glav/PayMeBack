718218b0fc117ff7ce1f546e1f301ba01cc76419
Paul Glavich <glav@theglavs.com>
2012-09-07 08:21:25 +1000

    more .Net4.5/MVC 4 migration changes

---
+++ PayMeBack/Glav.PayMeBack.Web/App_Start/MobileConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace Glav.PayMeBack.Web.App_Start
+{
+    public class MobileConfig
+    {
+        public static void Register()
+        {
+			DisplayModeProvider.Instance.Modes.Insert(0, new
+			DefaultDisplayMode("Mobile")
+			{
+				ContextCondition = (context =>
+				{
+					var userAgent = context.GetOverriddenUserAgent();
+					var useMobileView = (userAgent.IndexOf("Mobi", StringComparison.OrdinalIgnoreCase) >= 0) || (userAgent.IndexOf("iPhone", StringComparison.OrdinalIgnoreCase) >= 0);
+
+					// This is for debugging with Firefox - comment out for real use
+					//if (!useMobileView)
+					//{
+					//    useMobileView = userAgent.IndexOf("Mozilla", StringComparison.OrdinalIgnoreCase) >= 0;
+					//}
+
+					context.Items[MobileViewHelper.MobileContextKey] = useMobileView;
+					return useMobileView;
+				})
+			});
+
+        }
+    }
+}
