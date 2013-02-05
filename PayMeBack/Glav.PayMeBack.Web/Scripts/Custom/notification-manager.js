cf2d20b7003ee17aa1745ff4cb9e09f5c24a2cd6
glav <glav@localhost>
2013-02-05 17:08:14 +1100

    Basics of notification dialog

--- PayMeBack/Glav.PayMeBack.Web/Scripts/Custom/notification-manager.js
+++ PayMeBack/Glav.PayMeBack.Web/Scripts/Custom/notification-manager.js
@@ -9,6 +9,37 @@ if (typeof window.payMeBack.debtManager === 'undefined') {
 window.payMeBack.notificationManager = (function () {
 
     var showNotificationOptionsForDebt = function(competedCallback) {
+        $.nyroModalManual({
+            url: '#notification-options-modal',
+            minHeight: 300,
+            height: 400,
+            minWwidth: 500,
+            bgColor: window.payMeBack.core.colours.nyroModalBackground,
+            //modal: true,
+            //closeButton: null,
+            endRemove: function () {
+                $("#notification-options-container fieldset").show();
+                $("#notification-options-container .progress-indicator").hide();
+                if (typeof completionCallback !== 'undefined') {
+                    completionCallback();
+                }
+
+            },
+            endShowContent: function () {
+                var debtContainer = $("#notification-options-container");
+                $(".progress-indicator", debtContainer).hide();
+                $("fieldset ul li input", debtContainer).unbind().on("keypress", function (e) {
+                    if (e.which === 13) {
+                        captureNotificationFormAndSubmit();
+                    }
+                });
+                $("#notification-options-submit").unbind().on("click", function (e) {
+                    captureNotificationFormAndSubmit();
+                });
+
+                $("#add-debt-user-email").focus();
+            }
+        });
         alert('getting there, but this section is not complete');
     };
     
