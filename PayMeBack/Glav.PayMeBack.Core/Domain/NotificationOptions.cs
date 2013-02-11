5a5501e85a613df837d6fd55b0030ff01c3b5598
Paul Glavich <glav@theglavs.com>
2013-02-12 07:54:00 +1100

    Added some further enhancements to display notification options

--- PayMeBack/Glav.PayMeBack.Core/Domain/NotificationOptions.cs
+++ PayMeBack/Glav.PayMeBack.Core/Domain/NotificationOptions.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Glav.PayMeBack.Core.Domain
 {
     public class NotificationOptions : BaseModel
     {
+        public NotificationOptions()
+        {
+            Interval = new ReminderInterval();
+        }
         public Guid UserId { get; set; }
         public Guid DebtId { get; set; }
+
+        [Display(Description="Notification Method", ShortName="Method")]
         public NotificationType NotificationMethod { get; set; }
+        
+        [Display(Description="Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string NotificationEmailAddress { get; set; }
+
+        [Display(Description="Sms Number")]
         public string NotificationSmsNumber { get; set; }
+
         public ReminderInterval Interval { get; set; }
     }
 
