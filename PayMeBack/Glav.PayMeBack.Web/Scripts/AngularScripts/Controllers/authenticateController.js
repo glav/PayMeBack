a96b3c72aa2f2b40ef5eeca1e45da0eef1e81ffd
glav <glav@localhost>
2013-06-11 06:02:42 +1000

    Fixed issue with sign up form

--- PayMeBack/Glav.PayMeBack.Web/Scripts/AngularScripts/Controllers/authenticateController.js
+++ PayMeBack/Glav.PayMeBack.Web/Scripts/AngularScripts/Controllers/authenticateController.js
@@ -6,6 +6,9 @@ window.payMeBack.app.controller(window.payMeBack.core.dependencies.authenticateC
     init();
 
     $scope.submitCredentials = function () {
+        if ($scope.loginForm.$invalid) {
+            return;
+        }
         window.payMeBack.login.submitCredentials($scope.loginData, $scope.isSignupAction, function () {
             $rootScope.isSignupAction = false;
             location.assign(window.payMeBack.core.makePathFromVirtual("~/summary"));
